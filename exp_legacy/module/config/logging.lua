115a64dfad8564aaf52b75a711569c680a4d3cd2
Cooldude2606 <25043174+Cooldude2606@users.noreply.github.com>
2024-09-28 01:56:54 +0100

    Update all code styles

--- exp_legacy/module/config/logging.lua
+++ exp_legacy/module/config/logging.lua
@@ -2,7 +2,7 @@
 -- @config logging
 
 return {
-    file_name = 'log/logging.log',
+    file_name = "log/logging.log",
     rocket_launch_display = {
         [1] = true,
         [2] = true,
@@ -25,19 +25,19 @@ return {
         [1000000] = true,
         [2000000] = true,
         [5000000] = true,
-        [10000000] = true
+        [10000000] = true,
     },
     disconnect_reason = {
-        [defines.disconnect_reason.quit] = ' left the game',
-        [defines.disconnect_reason.dropped] = ' was dropped from the game',
-        [defines.disconnect_reason.reconnect] = ' is reconnecting',
-        [defines.disconnect_reason.wrong_input] = ' was having a wrong input',
-        [defines.disconnect_reason.desync_limit_reached] = ' had desync limit reached',
-        [defines.disconnect_reason.cannot_keep_up] = ' cannot keep up',
-        [defines.disconnect_reason.afk] = ' was afk',
-        [defines.disconnect_reason.kicked] = ' was kicked',
-        [defines.disconnect_reason.kicked_and_deleted] = ' was kicked and deleted',
-        [defines.disconnect_reason.banned] = ' was banned',
-        [defines.disconnect_reason.switching_servers] = ' is switching servers'
-    }
+        [defines.disconnect_reason.quit] = " left the game",
+        [defines.disconnect_reason.dropped] = " was dropped from the game",
+        [defines.disconnect_reason.reconnect] = " is reconnecting",
+        [defines.disconnect_reason.wrong_input] = " was having a wrong input",
+        [defines.disconnect_reason.desync_limit_reached] = " had desync limit reached",
+        [defines.disconnect_reason.cannot_keep_up] = " cannot keep up",
+        [defines.disconnect_reason.afk] = " was afk",
+        [defines.disconnect_reason.kicked] = " was kicked",
+        [defines.disconnect_reason.kicked_and_deleted] = " was kicked and deleted",
+        [defines.disconnect_reason.banned] = " was banned",
+        [defines.disconnect_reason.switching_servers] = " is switching servers",
+    },
 }
