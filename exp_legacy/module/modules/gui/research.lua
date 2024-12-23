444a7e2548fad9f5e6a854d9a3439eb0fdd2d3ff
PHIDIAS <phidias@aperx.org>
2024-12-24 03:25:26 +0900

    Update research.lua

--- exp_legacy/module/modules/gui/research.lua
+++ exp_legacy/module/modules/gui/research.lua
@@ -95,8 +95,8 @@ end
 
 local function research_notification(event)
     if config.inf_res[research.mod_set][event.research.name] then
-        if event.research.name == config.bonus_inventory[research.mod_set].name then
-            if event.research.level == config.bonus_inventory[research.mod_set].level + 1 then
+        if event.research.name == config.bonus_inventory.res[research.mod_set].name then
+            if event.research.level == config.bonus_inventory.res[research.mod_set].level + 1 then
                 -- Add run result to log
                 research_add_log()
             end
